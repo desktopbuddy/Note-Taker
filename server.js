e403b17f4391612fd02d9ab0a6277e1f2450a158
desktopbuddy <hanchondo21@gmail.com>
2023-09-03 23:07:16 -0700

    set up middleware and port listener

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// middleware for parsing different data types
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// public folder host
+app.use(express.static('public'));
+
+// PORT listener
+app.listen(PORT, () => {
+  console.log(`Server now running on port ${PORT}!`);
+});
