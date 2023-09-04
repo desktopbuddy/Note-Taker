77c7f8f36b119108599c02b908046c30b3658722
desktopbuddy <hanchondo21@gmail.com>
2023-09-03 23:57:08 -0700

    in html routes made path to index and notes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,2 +1,14 @@
 const path = require('path');
 const router = require('express').router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
