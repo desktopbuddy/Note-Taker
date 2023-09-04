44c7aceb05b664a72a9215ce451fec8fde6440aa
desktopbuddy <hanchondo21@gmail.com>
2023-09-04 00:17:42 -0700

    added index to apiroutes and fixed some errors

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const router = require('express').router();
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -12,3 +12,5 @@ router.get('/notes', (req, res) => {
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
+
+module.exports = router;
