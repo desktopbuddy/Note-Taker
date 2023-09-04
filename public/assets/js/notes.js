3c0867f9816cad190bca9426dde5e57fd8beda4b
desktopbuddy <hanchondo21@gmail.com>
2023-09-04 01:52:40 -0700

    finished delete function

--- public/assets/js/notes.js
+++ public/assets/js/notes.js
@@ -13,13 +13,25 @@ function createNewNote(body, notesArray) {
 
 function deleteNote(id, notes) {
     let notesArray = notes.filter(el => {
-        if (el.id == id) {
-            return false
-        }else {
-            return true
-        }
+      if (el.id == id) {
+        return false
+      } else {
+        return true
+      }
     });
-}
+  
+    let index = 0;
+    notesArray.forEach(note => {
+      note.id = index;
+      index += 1;
+    });
+  
+      fs.writeFileSync(
+        path.join(__dirname, '../../../db/db.json'),
+        JSON.stringify({ notesArray }, null, 2)
+      );
+      return notesArray;
+    }
 
 module.exports = {
     createNewNote,
